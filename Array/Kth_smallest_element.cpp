88c0a49dbea0eaee16bd62b6e07f6805624ca34b
ZeroDeath <zero.death.0313@gmail.com>
2022-01-02 23:55:21 +0530

    Kth smallest element cpp

---
+++ Array/Kth_smallest_element.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int kthSmallest(int arr[], int l, int r, int k) 
+    {
+        priority_queue<int> pq;
+        for(int i=0;i<=r;i++)
+        {
+            pq.push(arr[i]);
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
