b9382010e04ffd97346b9327e8cbf4d7272b74a3
ZeroDeath <zero.death.0313@gmail.com>
2022-01-06 01:45:20 +0530

    Minimum jumps to reach end cpp

---
+++ Array/Minimum_jumps_to_reach_end.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        int ans=1;
+        int maxjmp=arr[0];
+        int step=arr[0];
+        if(n==1)
+        {
+            return 0;
+        }
+        else if(arr[0]==0)
+        {
+            return -1;
+        }
+        else
+        {
+            for(int i=1;i<n;i++)
+            {
+                if(i==n-1)
+                {
+                    return ans;
+                }
+                maxjmp=max(maxjmp,i+arr[i]);
+                step--;
+                if(step==0)
+                {
+                    ans++;
+                    if(i>=maxjmp)
+                    {
+                        return -1;
+                    }
+                    step=maxjmp-i;
+                }
+            }
+        }
+    }
+};
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,j;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout<<obj.minJumps(arr, n)<<endl;
+    }
+    return 0;
+}
