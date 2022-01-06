c5942b5852b6b66f7d29fbbc57a59dfae8bc02f4
ZeroDeath <zero.death.0313@gmail.com>
2022-01-07 02:06:36 +0530

    Count inversions cpp

---
+++ Array/Count_inversions.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+  public:
+    void merge(long long arr[],long long l,long long mid,long long r,long long &count)
+    {
+        long long i=l,j=mid+1,k=l,temp[r];
+        while(i<=mid && j<=r)
+        {
+            if(arr[i]<=arr[j])
+            {
+                temp[k++]=arr[i++];
+            }
+            else
+            {
+                count+=(mid-i+1);
+                temp[k++]=arr[j++];
+            }
+        }
+        while(i<=mid)
+        {
+            temp[k++]=arr[i++];
+        }
+        while(j<=r)
+        {
+            temp[k++]=arr[j++];
+        }
+        for(int i=l;i<=r;i++)
+        {
+            arr[i]=temp[i];
+        }
+    }
+    void mergesort(long long arr[],long long l,long long r,long long &count)
+    {
+        if(l<r)
+        {
+            long long mid=(l+r)/2;
+            mergesort(arr,l,mid,count);
+            mergesort(arr,mid+1,r,count);
+            merge(arr,l,mid,r,count);
+        }
+    }
+    long long int inversionCount(long long arr[], long long N)
+    {
+        long long count=0;
+        mergesort(arr,0,N-1,count);
+        return count;
+    }
+
+};
+int main() {
+    
+    long long T;
+    cin >> T;
+    
+    while(T--){
+        long long N;
+        cin >> N;
+        
+        long long A[N];
+        for(long long i = 0;i<N;i++){
+            cin >> A[i];
+        }
+        Solution obj;
+        cout << obj.inversionCount(A,N) << endl;
+    }
+    
+    return 0;
+}
