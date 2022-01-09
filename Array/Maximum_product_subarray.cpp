20d1610e48e83ad56ccddf1c6cb0a840cf9f7378
ZeroDeath <zero.death.0313@gmail.com>
2022-01-10 01:09:56 +0530

    Maximum product subarray cpp

---
+++ Array/Maximum_product_subarray.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+	long long maxProduct(vector<int> arr, int n) {
+	    long long ans=INT_MIN,pre=1,post=1;
+	    for(int i=0;i<n;i++)
+	    {
+	        pre*=arr[i];
+	        post*=arr[n-i-1];
+	        ans=max(ans,max(pre,post));
+	        if(pre==0)
+	        {
+	            pre=1;
+	        }
+	        if(post==0)
+	        {
+	            post=1;
+	        }
+	    }
+	    return ans;
+	}
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
