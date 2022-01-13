9ac21630416041f4f2f3d851e0eccd5297665fef
ZeroDeath <zero.death.0313@gmail.com>
2022-01-13 16:32:55 +0530

    Median of 2 sorted arrays of equal size method 2 cpp

--- Array/Find_the_median.cpp
+++ Array/Find_the_median.cpp
@@ -18,3 +18,15 @@ public:
 		    return ans;
 		}
 };
+
+//Method 2
+int getMedian(int ar1[], int ar2[], int n)
+{
+    int j = 0;
+    int i = n - 1;
+    while (ar1[i] > ar2[j] && j < n && i > -1)
+        swap(ar1[i--], ar2[j++]);
+    sort(ar1, ar1 + n);
+    sort(ar2, ar2 + n);
+    return (ar1[n - 1] + ar2[0]) / 2;
+}
