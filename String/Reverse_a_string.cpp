767a0492f88608b915d8a8169e0e3cbf50e0cce9
ZeroDeath <zero.death.0313@gmail.com>
2022-01-17 20:52:58 +0530

    Reverse a string cpp

---
+++ String/Reverse_a_string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int low=0,high=s.size()-1;
+        while(low<high)
+        {
+            swap(s[low],s[high]);
+            low++;
+            high--;
+        }
+    }
+};
