12d3bc5865153f7a79749c0bc701089242d83611
ZeroDeath <zero.death.0313@gmail.com>
2022-02-07 02:41:35 +0530

    Value equal to index value cpp

---
+++ Searching & Sorting/Value_equal_to_index_value.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    vector<int> ans;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]==i+1)
+	        {
+	            ans.push_back(i+1);
+	        }
+	    }
+	    return ans;
+	}
+};
