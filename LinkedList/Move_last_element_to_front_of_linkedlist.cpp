b1a423c759ad7df8129ff340ff167f7a55c27f66
ZeroDeath <zero.death.0313@gmail.com>
2022-02-20 00:37:26 +0530

    Move last element to front of linked list cpp

---
+++ LinkedList/Move_last_element_to_front_of_linkedlist.cpp
@@ -0,0 +1,18 @@
+
+void moveToLast(node *head)
+{
+    if(*head==NULL || (*head)->next==NULL)
+    {
+        return;
+    }
+    node *seclast=NULL;
+    node *last=head;
+    while(last->next!=NULL)
+    {
+        seclast=last;
+        last=last->next;
+    }
+    seclast=NULL;
+    last->next=head;
+    head=last;
+}
