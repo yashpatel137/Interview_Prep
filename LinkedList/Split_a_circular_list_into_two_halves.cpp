ea4456b3af1bc551eaf606314c0a3f1d362aa5bb
ZeroDeath <zero.death.0313@gmail.com>
2022-02-22 20:41:29 +0530

    Split a circular linked list into two halves cpp

---
+++ LinkedList/Split_a_circular_list_into_two_halves.cpp
@@ -0,0 +1,18 @@
+void splitList(Node *head, Node **head1_ref, Node **head2_ref)
+{
+    Node *slow=head,*fast=head->next;
+    while(fast!=head && fast->next!=head)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    *head1_ref=head;
+    *head2_ref=slow->next;
+    slow->next=head;
+    Node *temp=*head2_ref;
+    while(temp->next!=head)
+    {
+        temp=temp->next;
+    }
+    temp->next=*head2_ref;
+}
