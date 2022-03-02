1a686ae1dc94c4a5949454291275123b3dd1215a
ZeroDeath <zero.death.0313@gmail.com>
2022-03-02 15:49:45 +0530

    Preorder traversal cpp

---
+++ Binary Trees/Preorder_traversal.cpp
@@ -0,0 +1,16 @@
+void solve(Node *root,vector<int>&ans)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    ans.push_back(root->data);
+    solve(root->left,ans);
+    solve(root->right,ans);
+}
+vector <int> preorder(Node* root)
+{
+    vector<int> ans;
+    solve(root,ans);
+    return ans;
+}
