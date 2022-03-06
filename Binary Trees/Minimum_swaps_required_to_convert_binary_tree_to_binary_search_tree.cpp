e5ff640d0c14ec254400d2a091981038fbc76eac
ZeroDeath <zero.death.0313@gmail.com>
2022-03-07 00:19:29 +0530

    Minimum swaps required to convert bt to bst cpp

---
+++ Binary Trees/Minimum_swaps_required_to_convert_binary_tree_to_binary_search_tree.cpp
@@ -0,0 +1,29 @@
+void inorder(int a[], std::vector<int> &v,int n, int index)
+{
+	if(index >= n)
+		return;
+	inorder(a, v, n, 2 * index + 1);
+	v.push_back(a[index]);
+	inorder(a, v, n, 2 * index + 2);
+}
+int minSwaps(std::vector<int> &v)
+{
+	std::vector<pair<int,int> > t(v.size());
+	int ans = 0;
+	for(int i = 0; i < v.size(); i++)
+		t[i].first = v[i], t[i].second = i;
+	
+	sort(t.begin(), t.end());
+	for(int i = 0; i < t.size(); i++)
+	{
+		if(i == t[i].second)
+			continue;
+		else
+		{
+            ans++;
+			swap(t[i],t[t[i].second]);
+            i--;
+		}
+	}
+	return ans;
+}
