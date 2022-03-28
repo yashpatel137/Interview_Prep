4fcc22042731c3c270d1533ed09f132730ca61be
ZeroDeath <zero.death.0313@gmail.com>
2022-03-28 17:14:40 +0530

    Program for Shortest Job First (or SJF) CPU Scheduling | Set 1 (Non- preemptive) cpp

---
+++ Greedy/Program_for_shortest_job_first_CPU_scheduling.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comp(vector<int> a, vector<int> b)
+{
+  if (a[1] == b[1])
+  {
+    if (a[2] == b[2])
+    {
+      return a[0] < b[0];
+    }
+    else
+    {
+      return a[2] < b[2];
+    }
+  }
+  return a[1] < b[1];
+}
+
+void init_code()
+{
+#ifndef ONLINE_JUDGE
+  freopen("Input.txt", "r", stdin);
+  freopen("Output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+  init_code();
+  int n;
+  cin >> n;
+  vector<vector<int>> v(n);
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      int x;
+      cin >> x;
+      v[i].push_back(x);
+    }
+  }
+  vector<int> ans;
+  sort(v.begin(), v.end(), comp);
+  priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+  pq.push({v[0][2], v[0][1], v[0][0]});
+  int i = 1;
+  int currtime = 0;
+  while (!pq.empty())
+  {
+    int btime = pq.top()[0];
+    int id = pq.top()[2];
+    pq.pop();
+    ans.push_back(id);
+    currtime += btime;
+    while (true)
+    {
+      if (i < n and v[i][1] <= currtime)
+      {
+        pq.push({v[i][2], v[i][1], v[i][0]});
+        i++;
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+  for (int i = 0; i < ans.size(); i++)
+  {
+    cout << ans[i] << " ";
+  }
+}
