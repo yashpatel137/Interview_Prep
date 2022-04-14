9089aef8d494fe1dd9045d4f71a3c20d599abf99
ZeroDeath <zero.death.0313@gmail.com>
2022-04-14 13:30:54 +0530

    Expression contains redundant bracket or not cpp

---
+++ Stacks&Queues/Expression_contains_redundant_brackets_or_not.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool checkRedundancy(string& str)
+{
+	stack<char> st;
+	for (auto& ch : str) {
+		if (ch == ')') {
+			char top = st.top();
+			st.pop();
+			bool flag = true;
+
+			while (!st.empty() and top != '(') 
+            {
+				if (top == '+' || top == '-' || top == '*' || top == '/')
+					flag = false;
+				top = st.top();
+				st.pop();
+			}
+			if (flag == true)
+				return true;
+		}
+
+		else
+			st.push(ch); 
+	}
+	return false;
+}
+void findRedundant(string& str)
+{
+	bool ans = checkRedundancy(str);
+	if (ans == true)
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+}
+int main()
+{
+	string str = "((a+b))";
+	findRedundant(str);
+
+	str = "(a+(b)/c)";
+	findRedundant(str);
+
+	str = "(a+b*(c-d))";
+	findRedundant(str);
+
+	return 0;
+}
