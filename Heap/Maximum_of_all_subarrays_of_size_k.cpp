2a3bbadb939ecf105c42047b182b22d45b2806b4
ZeroDeath <zero.death.0313@gmail.com>
2022-04-21 11:11:54 +0530

    Sliding window maximum cpp

--- Heap/Maximum_of_all_subarrays_of_size_k.cpp
+++ Heap/Maximum_of_all_subarrays_of_size_k.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>>pq;
-        vector<int>ans;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    {
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
         int n=nums.size();
-        
-        for(int i=0;i<n;i++){
-            if(i==k)ans.push_back(pq.top().first);
-            pq.push({nums[i],i});
-         
-            if(k==n&&i==n-1)ans.push_back(pq.top().first);
-            if(i<k)continue;
-        
-            while(pq.top().second<i-k+1){
-               pq.pop(); 
+        for(int i=0;i<n;i++)
+        {
+            if(i==k)
+            {
+                ans.push_back(pq.top().first);
             }
-           ans.push_back(pq.top().first);
+            pq.push({nums[i],i});
+            if(k==n && i==n-1)
+            {
+                ans.push_back(pq.top().first);
+            }
+            if(i<k)
+            {
+                continue;
+            }
+            while(!pq.empty() && pq.top().second<i-k+1)
+            {
+                pq.pop();
+            }
+            ans.push_back(pq.top().first);
         }
-        
         return ans;
     }
 };
