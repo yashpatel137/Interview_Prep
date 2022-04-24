5a14e5d410a459a8bfcb0fd54378805612d3d1c8
ZeroDeath <zero.death.0313@gmail.com>
2022-04-24 16:55:28 +0530

    Kth largest sum contigous subarray cpp

--- Heap/Kth_largest_sum_contigous_subarray.cpp
+++ Heap/Kth_largest_sum_contigous_subarray.cpp
@@ -1,37 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
-int kthLargestSum(int arr[], int n, int k)
+int getKthLargest(vector<int> &arr, int k)
 {
-	int sum[n + 1];
-	sum[0] = 0;
-	sum[1] = arr[0];
-	for (int i = 2; i <= n; i++)
-		sum[i] = sum[i - 1] + arr[i - 1];
-	priority_queue<int, vector<int>, greater<int> > Q;
-	for (int i = 1; i <= n; i++)
+	int n=arr.size();
+	vector<int> sum(n+1);
+	sum[0]=0;
+	sum[1]=arr[0];
+	for(int i=2;i<=n;i++)
 	{
-		for (int j = i; j <= n; j++)
+		sum[i]=sum[i-1]+arr[i-1];
+	}
+	priority_queue<int,vector<int>,greater<int>>pq;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i;j<=n;j++)
 		{
-			int x = sum[j] - sum[i - 1];
-			if (Q.size() < k)
-				Q.push(x);
+			int temp=sum[j]-sum[i-1];
+			if(pq.size()<k)
+			{
+				pq.push(temp);
+			}
 			else
 			{
-				if (Q.top() < x)
+				if(pq.top()<temp)
 				{
-					Q.pop();
-					Q.push(x);
+					pq.pop();
+					pq.push(temp);
 				}
 			}
 		}
 	}
-	return Q.top();
-}
-int main()
-{
-	int a[] = { 10, -10, 20, -40 };
-	int n = sizeof(a) / sizeof(a[0]);
-	int k = 6;
-	cout << kthLargestSum(a, n, k);
-	return 0;
+	return pq.top();
 }
