4ff971a6c693b669075d64f71598dccbd564b259
ZeroDeath <zero.death.0313@gmail.com>
2022-04-27 17:24:07 +0530

    Min heap to max heap cpp

---
+++ Heap/Convert_min_Heap_to_max_heap.cpp
@@ -0,0 +1,20 @@
+void MaxHeapify(int arr[], int i, int n)
+{
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    int largest = i;
+    if (l < n && arr[l] > arr[i])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        MaxHeapify(arr, largest, n);
+    }
+}
+void convertMaxHeap(int arr[], int n)
+{
+    for (int i = (n-2)/2; i >= 0; --i)
+        MaxHeapify(arr, i, n);
+}
