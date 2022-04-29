e52bfc1a8412cfc54c5058eec47c945a447bd218
ZeroDeath <zero.death.0313@gmail.com>
2022-04-29 10:42:54 +0530

    Detect cycle in directed graph using BFS cpp

---
+++ Graph/Detect_cycle_in_directed_graph_BFS.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+	bool isCyclic(int N, vector<int> adj[]) {
+        queue<int> q; 
+	    vector<int> indegree(N, 0); 
+	    for(int i = 0;i<N;i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i = 0;i<N;i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+	    int cnt = 0;
+	    while(!q.empty()) {
+	        int node = q.front(); 
+	        q.pop(); 
+	        cnt++; 
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+	    if(cnt == N) return false; 
+	    return true; 
+	}
+};
