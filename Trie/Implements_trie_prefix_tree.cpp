7a395dfaf7aa6bf1687eafeb1709f47c300d81fd
ZeroDeath <zero.death.0313@gmail.com>
2022-05-15 15:38:58 +0530

    Implements trie cpp

---
+++ Trie/Implements_trie_prefix_tree.cpp
@@ -0,0 +1,77 @@
+struct Node
+{
+    Node *links[26];
+    bool flag=false;
+    bool containsKey(char c)
+    {
+        return (links[c-'a']!=NULL);
+    }
+    void put(char ch, Node *node)
+    {
+        links[ch-'a']=node;
+    }
+    Node *get(char ch)
+    {
+        return links[ch-'a'];
+    }
+    void setEnd()
+    {
+        flag=true;
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+class Trie 
+{
+private: 
+    Node *root;
+public:
+    Trie() 
+    {
+        root=new Node();
+    }
+    
+    void insert(string word) 
+    {
+        Node *node=root;
+        for(int i=0;i<word.size();i++)
+        {
+            if(!node->containsKey(word[i]))
+            {
+                node->put(word[i],new Node());
+            }
+            node=node->get(word[i]);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) 
+    {
+        Node *node=root;
+        for(int i=0;i<word.size();i++)
+        {
+            if(!node->containsKey(word[i]))
+            {
+                return false;
+            }
+            node=node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) 
+    {
+        Node *node=root;
+        for(int i=0;i<prefix.length();i++)
+        {
+            if(!node->containsKey(prefix[i]))
+            {
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
