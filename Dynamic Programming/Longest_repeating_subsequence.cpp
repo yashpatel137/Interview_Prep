b40571c0d1d3d5df34ab1fbaf06ae34f66609af3
ZeroDeath <zero.death.0313@gmail.com>
2022-05-23 10:34:04 +0530

    Longest repeating subsequence cpp

---
+++ Dynamic Programming/Longest_repeating_subsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str)
+		{
+		    int n=str.length();
+		    int dp[n+1][n+1];
+		    for(int i=0;i<=n;i++)
+		    {
+		        for(int j=0;j<=n;j++)
+		        {
+		            if(i==0||j==0)
+		            {
+		                dp[i][j]=0;
+		            }
+		            else
+		            {
+		                if(str[i-1]==str[j-1] and i!=j)
+		                {
+		                    dp[i][j]=1+dp[i-1][j-1];
+		                }
+		                else
+		                {
+		                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		                }
+		            }
+		        }
+		    }
+		    return dp[n][n];
+		}
+
+};
