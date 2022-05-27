59467a5d4ce3e9fadd2204991cf8161837ff2761
ZeroDeath <zero.death.0313@gmail.com>
2022-05-27 15:50:51 +0530

    Longest common substring cpp

---
+++ Dynamic Programming/Longest_common_substring.cpp
@@ -0,0 +1,29 @@
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        int dp[n+1][m+1];
+        int ans=0;
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(S1[i-1]==S2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    ans=max(ans,dp[i][j]);
+                }
+                else
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return ans;
+    }
+};
