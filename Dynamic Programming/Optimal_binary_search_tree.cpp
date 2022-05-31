584fa35b38dd76eeee4df3c91e2b8e0561c5372a
ZeroDeath <zero.death.0313@gmail.com>
2022-05-31 12:24:48 +0530

    Optimal binary search tree cpp

--- Dynamic Programming/Optimal_binary_search_tree.cpp
+++ Dynamic Programming/Optimal_binary_search_tree.cpp
@@ -1,3 +1,39 @@
+//Method 1 DP
+class Solution{
+    public:
+    int dp[101][101];
+    int solve(int i,int j,int freq[])
+    {
+        if(i>j)
+        {
+            return 0;
+        }
+        if(i==j)
+        {
+            return freq[i];
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int curr=accumulate(freq+i,freq+j+1,0);
+        int mn=INT_MAX;
+        for(int k=i;k<=j;k++)
+        {
+            int left=solve(i,k-1,freq);
+            int right=solve(k+1,j,freq);
+            mn=min(mn,left+right);
+        }
+        return dp[i][j]=curr+mn;
+    }
+    int optimalSearchTree(int keys[], int freq[], int n)
+    {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,n-1,freq);
+    }
+};
+
+//Method 2
 #include <bits/stdc++.h>
 using namespace std;
 int sum(int freq[], int i, int j);
