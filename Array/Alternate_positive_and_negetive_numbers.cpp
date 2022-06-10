8334f3f8c97df99acfe0b3d9545babecf23dac1c
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-06-10 12:10:27 +0530

    Efficient approach.

--- Array/Alternate_positive_and_negetive_numbers.cpp
+++ Array/Alternate_positive_and_negetive_numbers.cpp
@@ -49,3 +49,26 @@ int main() {
     }
     return 0;
 }
+
+//Method 2
+void rearrange(int arr[], int n)
+{
+    int i = 0, j = n-1;
+    while (i < j) {
+        while (i <= n - 1 and arr[i] > 0)
+            i += 1;
+        while (j >= 0 and arr[j] < 0)
+            j -= 1;
+        if (i < j )
+            swap(arr[i], arr[j]);
+    }
+    if (i == 0 || i == n)
+        return;
+    int k = 0;
+    while (k < n && i < n ) 
+    { 
+        swap(arr[k], arr[i]);
+        i = i + 1;
+        k = k + 2;
+    }
+}
