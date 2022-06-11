d8c2b032c8d484e719207e0547e82c08ce620047
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-06-11 14:38:41 +0530

    Update Kth_element_in_matrix.cpp

--- Matrix/Kth_element_in_matrix.cpp
+++ Matrix/Kth_element_in_matrix.cpp
@@ -40,3 +40,50 @@ int kthSmallest(int mat[MAX][MAX], int n, int k)
     }
     return low;
 }
+
+//Mdethod 3
+int getElementsGreaterThanOrEqual(int mat[MAX][MAX],int n,int num) 
+{
+    int ans = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        if (mat[i][0] > num) 
+        {
+            return ans;
+        }
+        if (mat[i][n - 1] <= num) 
+        {
+            ans += n;
+            continue;
+        }
+        int greaterThan = 0;
+        for (int jump = n / 2; jump >= 1; jump /= 2) 
+        {
+            while (greaterThan + jump < n && mat[i][greaterThan + jump] <= num) 
+            {
+                greaterThan += jump;
+            }
+        }
+        ans += greaterThan + 1;
+    }
+    return ans;
+}
+int kthSmallest(int mat[MAX][MAX], int n, int k)
+{
+    int low=mat[0][0];
+    int high=mat[n-1][n-1];
+    while(low<high)
+    {
+        int mid=(low+high)/2;
+        int count=(upperbound(mat,n,mid);
+        if(count<k)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid;
+        }
+    }
+    return low;
+}
