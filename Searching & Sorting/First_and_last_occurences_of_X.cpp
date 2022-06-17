e561ce1a1c6f830f1dbbde8c8248ca29dcda7f6d
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-06-17 11:04:32 +0530

    Update First_and_last_occurences_of_X.cpp

--- Searching & Sorting/First_and_last_occurences_of_X.cpp
+++ Searching & Sorting/First_and_last_occurences_of_X.cpp
@@ -31,3 +31,37 @@ vector<int> find(int arr[], int n , int x )
     }
     return ans;
 }
+
+//Method 2
+int getPosition(int arr[], int n, int x, string pos)
+{
+   int start = 0, end = n-1, position = -1;
+   while(start<=end)
+   {
+       int mid = (start+end)/2;
+       if(arr[mid] == x)
+       {
+           position = mid;
+           if(pos=="first")
+               end = mid-1;
+           else
+               start = mid+1;
+       }
+       else if(arr[mid]>x)
+       {
+           end = mid-1;
+       }
+       else
+       {
+           start = mid+1;
+       }
+   }
+   return position;
+}
+vector<int> find(int arr[], int n , int x )
+{
+   int first = getPosition(arr,n,x,"first");
+   int last = getPosition(arr,n,x,"last");
+   
+   return {first,last};
+}
