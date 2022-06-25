81c36718b03d1ce21968a95f02749d4c3df69fc6
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-06-26 01:04:46 +0530

    Update Deletion_from_a_circular_linked_list.cpp

--- LinkedList/Deletion_from_a_circular_linked_list.cpp
+++ LinkedList/Deletion_from_a_circular_linked_list.cpp
@@ -31,3 +31,30 @@ void deleteNode(Node** head, int key)
     else
         cout<<"no such keyfound";
 }
+
+
+//Method 2
+void deleteNode(struct Node **head, int key)
+{
+    Node *temp=*head;
+    while(temp->next->data!=key)
+    {
+        temp=temp->next;
+    }
+    Node *del=temp->next;
+    temp->next=temp->next->next;
+    delete(del);
+}
+ void reverse(struct Node** head_ref)
+{
+    Node *curr=*head_ref,*next,*prev=NULL;
+    while(curr->next!=NULL)
+    {
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    curr->next=prev;
+    *head_ref=prev;
+}
