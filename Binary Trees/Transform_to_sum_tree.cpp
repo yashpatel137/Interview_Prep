d8e8df2c2819bfe2a9ad07cd47869a387f5b3051
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-03 11:57:37 +0530

    Update Transform_to_sum_tree.cpp

--- Binary Trees/Transform_to_sum_tree.cpp
+++ Binary Trees/Transform_to_sum_tree.cpp
@@ -17,3 +17,17 @@ class Solution {
         solve(node);
     }
 };
+
+//Method 2
+int solve(Node *node){
+        if(!node)  return 0;
+        
+        int temp=node->data;
+        node->data=solve(node->right)+solve(node->left);
+        
+        return temp+node->data;
+    }
+    void toSumTree(Node *node)
+    {
+      solve(node);
+    }
