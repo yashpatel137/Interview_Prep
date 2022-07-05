98ac64cac272b6d37b56bf8fc8a83c234c2ecad9
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-05 11:37:04 +0530

    Update Lowest_common_ancestor_in_a_binary_tree.cpp

--- Binary Trees/Lowest_common_ancestor_in_a_binary_tree.cpp
+++ Binary Trees/Lowest_common_ancestor_in_a_binary_tree.cpp
@@ -28,3 +28,46 @@ class Solution
         }
     }
 };
+
+//Method 2 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q,bool &f1,bool &f2) {
+        if(!root) return NULL;
+        else if(root==p){
+            f1 = true;
+            return root;
+        }
+        else if(root==q){
+            f2 = true;
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left,p,q,f1,f2);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q,f1,f2);
+        
+        if(!left && !right) return NULL;
+        else if(left && right) return root;
+        else return left ? left:right;
+    }
+    bool findNode(TreeNode* root,TreeNode* target){
+        if(root==NULL){
+            return false;
+        }
+        if(root==target){
+            return true;
+        }
+        return findNode(root->left,target)||findNode(root->right,target);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(!root || !p || !q){
+            return NULL;
+        }
+        bool f1=false ,f2=false;
+        TreeNode* temp = lowestCommonAncestor(root,p,q,f1,f2);
+        if(!temp) return NULL;
+        if(f1 && !f2 && !findNode(temp,q)) return NULL;
+        if(!f1 && f2 && !findNode(temp,p)) return NULL;
+        return temp;
+    }
+};
