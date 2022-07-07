09f3815e9480178f5baa11372651ba93dee08ce2
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-07 11:33:44 +0530

    Update Delete_node_in_binary_search_tree.cpp

--- Binary Search Tree/Delete_node_in_binary_search_tree.cpp
+++ Binary Search Tree/Delete_node_in_binary_search_tree.cpp
@@ -49,3 +49,59 @@ public:
         }
     }
 };
+
+//Method 2 faster
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) 
+    {
+        if(!root)
+        {
+            return root;
+        }
+        if(root->val<key)
+        {
+            root->right=deleteNode(root->right,key);
+            return root;
+        }
+        else if(root->val>key)
+        {
+            root->left=deleteNode(root->left,key);
+            return root;
+        }
+        else
+        {
+            TreeNode *keynode=root;
+            if(!root->left and !root->right)
+            {
+                delete(root);
+                return NULL;
+            }
+            else if(!root->left)
+            {
+                TreeNode* temp=root->right;
+                delete(root);
+                return temp;
+            }
+            else if(!root->right)
+            {
+                TreeNode* temp=root->left;
+                delete(root);
+                return temp;
+            }
+            else
+            {
+                TreeNode *rightmin=root->right;
+                while(rightmin->left)
+                {
+                    rightmin=rightmin->left;
+                }
+                rightmin->left=keynode->left;
+                root=root->right;
+                delete(keynode);
+                return root;
+            }      
+            
+        }
+    }
+};
