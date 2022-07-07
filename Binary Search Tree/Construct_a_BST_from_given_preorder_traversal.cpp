d3d2c4134d7772974fcb0276670a92e16b107852
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-07 12:34:14 +0530

    Update Construct_a_BST_from_given_preorder_traversal.cpp

--- Binary Search Tree/Construct_a_BST_from_given_preorder_traversal.cpp
+++ Binary Search Tree/Construct_a_BST_from_given_preorder_traversal.cpp
@@ -1,3 +1,5 @@
+//Method 1 O(n)
+
 node* constructTreeUtil(int pre[], int* preIndex, int key,int min, int max, int size)
 {
     if (*preIndex >= size)
@@ -22,4 +24,52 @@ node* constructTree(int pre[], int size)
 {
     int preIndex = 0;
     return constructTreeUtil(pre, &preIndex, pre[0],INT_MIN, INT_MAX, size);
+}
+
+//Method 2 O(n^2)
+
+class Node {
+  public:
+  int data;
+  Node* left;
+  Node* right;
+  Node(int data)
+  {
+    this->data = data;
+    this->left = this->right = NULL;
+  }
+};
+static Node* node;
+Node* createNode(Node* node, int data)
+{
+  if (node == NULL)
+    node = new Node(data);
+ 
+  if (node->data > data)
+    node->left = createNode(node->left, data);
+  if (node->data < data)
+    node->right = createNode(node->right, data);
+ 
+  return node;
+}
+void create(int data)
+{
+  node = createNode(node, data);
+}
+void inorderRec(Node* root)
+{
+  if (root != NULL) {
+    inorderRec(root->left);
+    cout<<root->data<<endl;
+    inorderRec(root->right);
+  }
+}
+int main()
+{
+  vector<int> nodeData = { 10, 5, 1, 7, 40, 50 };
+ 
+  for (int i = 0; i < nodeData.size(); i++) {
+    create(nodeData[i]);
+  }
+  inorderRec(node);
 }
