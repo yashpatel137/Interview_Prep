d7842e475bdd55d5f02573142b42a7ecdfd08a0d
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-10 11:33:32 +0530

    Update Minimum_number_of_coins.cpp

--- Greedy/Minimum_number_of_coins.cpp
+++ Greedy/Minimum_number_of_coins.cpp
@@ -13,3 +13,30 @@ int findMin(int deno[], int V)
     }
     return count;
 }
+
+//Method 2
+class Solution{
+
+	public:
+	int minCoins(int coins[], int n, int v) 
+	{ 
+	    if(v==0)
+	    {
+	        return 0;
+	    }
+	    int count=INT_MAX;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(v>=coins[i])
+	        {
+	            int c=minCoins(coins,n,v-coins[i]);
+	            if(c!=INT_MAX and c+1<count)
+	            {
+	                count=c+1;
+	            }
+	        }
+	    }
+	    return count;
+	} 
+	  
+};
