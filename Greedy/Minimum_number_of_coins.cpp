e36daf5877b491b7a5eb88f32d850d6848d6ace6
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-10 11:36:09 +0530

    Update Minimum_number_of_coins.cpp

--- Greedy/Minimum_number_of_coins.cpp
+++ Greedy/Minimum_number_of_coins.cpp
@@ -1,17 +1,20 @@
-int findMin(int deno[], int V)
+int deno[] = { 1, 2, 5, 10, 20,
+               50, 100, 500, 1000 };
+int n = sizeof(deno) / sizeof(deno[0]);
+  
+vector<int> findMin(int V)
 {
-    int n=sizeof(deno)/sizeof(deno[0]);
-    sort(deno,deno+n);
-    int count=0;
-    for(int i=n-1;i>=0;i--)
+    sort(deno, deno + n);
+    vector<int> ans;
+    for (int i = n - 1; i >= 0; i--) 
     {
-        while(V>=deno[i])
-        {
-            V-=deno[i];
-            count++;
+        while (V >= deno[i]) 
+	{
+            V -= deno[i];
+            ans.push_back(deno[i]);
         }
     }
-    return count;
+	return ans;
 }
 
 //Method 2
