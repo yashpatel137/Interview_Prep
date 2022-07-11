70cfce08ab93e1bef990635525e68f4e7d6e6745
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-11 12:48:57 +0530

    Update Check_if_it_is_possible_to_survive_on_island.cpp

--- Greedy/Check_if_it_is_possible_to_survive_on_island.cpp
+++ Greedy/Check_if_it_is_possible_to_survive_on_island.cpp
@@ -18,3 +18,22 @@ int main()
 	survival(S, N, M);
 	return 0;
 }
+
+//Method 2
+class Solution{
+public:
+    int minimumDays(int S, int N, int M){
+        if(M>N)
+        {
+            return -1;
+        }
+        int totFood=S*M;
+        int totalAvail=S-S/7;
+        int reqDays=ceil(totFood/(float)(N));
+        if(reqDays<=totalAvail)
+        {
+            return reqDays;
+        }
+        return -1;
+    }
+};
