00b87a3f615bd78bae5983fee7fb964828762222
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-11 14:54:04 +0530

    Update Maximum_product_subarray.cpp

--- Greedy/Maximum_product_subarray.cpp
+++ Greedy/Maximum_product_subarray.cpp
@@ -19,3 +19,44 @@ public:
 	    return ans;
 	}
 };
+
+//Method 2
+class Solution{
+    public:
+    long long int mod=1e9+7;
+    long long int findMaxProduct(vector<int>&a, int n){
+        if(n==1)
+        {
+            return a[0];
+        }
+        int maxneg=INT_MIN,zeroc=0,negc=0;
+        long long int prod=1;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==0)
+            {
+                zeroc++;
+               continue;
+            }
+            if(a[i]<0)
+            {
+                negc++;
+                maxneg=max(maxneg,a[i]);
+            }
+            prod=(prod*a[i])%mod;
+        }
+        if(zeroc==n)
+        {
+            return 0;
+        }
+        if(negc%2!=0)
+        {
+            if(negc==1 and zeroc>0 and zeroc+negc==n)
+            {
+                return 0;
+            }
+            return (prod/maxneg);
+        }
+        return prod;
+    }
+};
